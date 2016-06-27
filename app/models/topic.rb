dffcb741887226ae24387f6f4dd9efed1054d3da
YengTsan <Yengttt@gmail.com>
2016-06-27 17:08:31 +0800

    Adjust syntax style in Index view and Topic model

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -15,16 +15,12 @@ class Topic < ActiveRecord::Base
   belongs_to :user
 
   def self.sorted_by_lasted_updated
-    @topics = Topic.all.sort_by do |t|
-      t.comments.exists? ? t.comments.last.updated_at : t.updated_at
-    end
+    @topics = Topic.all.sort_by { |t| t.comments.exists? ? t.comments.last.updated_at : t.updated_at }
     @topics.reverse
   end
 
   def self.sorted_by_number_of_comments
-    @topics = Topic.all.sort_by do | topic |
-      Comment.count_by_topic_id( topic.id )
-    end
+    @topics = Topic.all.sort_by { | topic | Comment.count_by_topic_id( topic.id ) }
     @topics.reverse
   end
 
